6fef58c5ca61306f7c5bca270ee2bcaa1a35684a
tatsuya <tatsuya.kigi@gmail.com>
2021-03-17 18:10:09 +0900

    apply error pages

---
+++ nuxt/pages/error.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  v-row(justify="center", align="center")
+    v-col(cols="12", sm="10", md="10")
+      div
+        v-alert(type="error", border="left", colored-border, elevation="2")
+          | このページにはアクセスできません。 必要であれば、再度ログインしてください。
+      div.text-center.to-top
+        a(href="/")
+          | ログインページへ
+
+</template>
+
+<script>
+export default {
+  layout() {
+    return 'notlogin'
+  },
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+<style>
+div.to-top {
+  margin-top: 2rem;
+  font-size: 1.5rem;
+}
+</style>
